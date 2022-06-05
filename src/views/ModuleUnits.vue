da5242e6530d744019fdcba2c3c42743e09940fa
mug1wara26 <aloysiusgsq@gmail.com>
2022-06-05 17:11:13 +0800

    Made units grid

--- src/views/ModuleUnits.vue
+++ src/views/ModuleUnits.vue
@@ -1,7 +1,11 @@
 <template>
   <v-container class="pa-6" max-width="80rem" fluid>
-    <v-card class="px-12 py-6" max-width="80rem">
-      <router-link to="/modules" class="back-link">
+    <v-card
+      class="px-12 py-6"
+      max-width="80rem"
+      elevation="0"
+    >
+      <router-link to="/modules" class="links">
         < My modules
       </router-link> <br/>
 
@@ -27,8 +31,39 @@
 
     </v-card>
 
-    <div class="pa-15">
-      <p>Units - {{ completed_units }}/{{ total_units }} completed</p>
+    <div class="pa-15" id="unit-grid">
+      <b id="completed-units">Units • {{ completed_units }}/{{ total_units }} completed </b>
+      <p/>
+
+      <v-row>
+        <v-col
+          cols="12"
+          lg="4"
+          md="6"
+          v-for="(unit, index) in units"
+          :key="index"
+        >
+          <router-link
+            class="unit-card"
+            :to="'/modules/' + $route.params.module_id + '/' + unit.unit_no"
+          >
+            <v-card
+              class="pa-4"
+              elevation="0"
+            >
+              <h2>
+                {{ unit.unit_title }}
+              </h2>
+              <p class="card-subtitle">
+                <b>{{ unit.sections_completed }}/{{ unit.total_sections }}</b> sections completed
+              </p>
+              <br/>
+              <p id="learn-arrow">Learn →</p>
+            </v-card>
+          </router-link>
+        </v-col>
+      </v-row>
+
 
       <v-btn :to="'/modules/' + $route.params.module_id + '/about'">
         About this module
@@ -52,37 +87,44 @@ export default Vue.extend({
   }),
   computed: {
     units(): Array<{
+      unit_no: number;
       unit_title: string;
       sections_completed: number;
       total_sections: number;
     }> {
       return [
         {
+          unit_no: 1,
           unit_title: "Unit 1",
           sections_completed: 3,
           total_sections: 3,
         },
         {
+          unit_no: 2,
           unit_title: "Unit 2",
           sections_completed: 3,
           total_sections: 3,
         },
         {
+          unit_no: 3,
           unit_title: "Unit 3",
           sections_completed: 3,
           total_sections: 3,
         },
         {
+          unit_no: 4,
           unit_title: "Unit 4",
           sections_completed: 3,
           total_sections: 3,
         },
         {
+          unit_no: 5,
           unit_title: "Unit 5",
           sections_completed: 3,
           total_sections: 3,
         },
         {
+          unit_no: 6,
           unit_title: "Unit 6",
           sections_completed: 3,
           total_sections: 3,

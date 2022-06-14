fe4efbdc37b2ee917cd7d5a87050a06923deccbd
mug1wara26 <aloysiusgsq@gmail.com>
2022-06-14 22:45:04 +0800

    barebones calendar

--- src/views/Schedule.vue
+++ src/views/Schedule.vue
@@ -2,7 +2,18 @@
   <v-container fluid class="pl-20 pr-31 py-16">
     <h1>Schedule</h1>
 
-    <v-row class="mt-4">
+    <v-row class="mt-4 d-flex align-center">
+      <v-col cols="1">
+        <v-btn
+          outlined
+          class="mr-4"
+          color="grey darken-2"
+          @click="setToday"
+        >
+          Today
+        </v-btn>
+      </v-col>
+
       <v-col cols="1">
         <v-btn
           icon
@@ -44,7 +55,7 @@
     <v-sheet height="600">
       <v-calendar
         ref="calendar"
-        v-model="value"
+        v-model="focus"
         :weekdays="weekday"
         :type="type.toLowerCase()"
       ></v-calendar>
@@ -57,16 +68,21 @@
 import Vue from "vue";
 
 export default Vue.extend({
-    name: "Schedule",
-    data: () => ({
-      type: "month",
-      types: ["Month", "Week", "Day", "4Day"],
-      value: "",
-      weekday: [0, 1, 2, 3, 4, 5, 6],
-      select: "All Modules",
-      items: ["All Modules", "Module 1", "Module 2", "Module 3"],
-    }),
-  });
+  name: "Schedule",
+  data: () => ({
+    type: "month",
+    types: ["Month", "Week", "Day", "4Day"],
+    focus: "",
+    weekday: [0, 1, 2, 3, 4, 5, 6],
+    select: "All Modules",
+    items: ["All Modules", "Module 1", "Module 2", "Module 3"],
+  }),
+  methods: {
+    setToday() {
+      this.focus = "";
+    },
+  },
+});
 </script>
 
 <style lang="scss" scoped>

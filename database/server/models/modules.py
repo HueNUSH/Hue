4b03ae26b85837942e4bb56733f6a9b5852594c2
mug1wara26 <aloysiusgsq@gmail.com>
2022-06-29 00:07:29 +0800

    Added modules, units and sections schema

---
+++ database/server/models/modules.py
@@ -0,0 +1,32 @@
+from typing import Literal
+
+from pydantic import BaseModel, Field
+
+
+# TODO : Validators for string length
+class UnitSections(BaseModel):
+    sectionId: int = Field(...)
+    sectionName: str = Field(...)
+    sectionSlug: str = Field(...)
+    sectionDesc: str = Field(...)
+    mediaType: Literal['pdf', 'html' 'video'] = Field(...)
+    sectionMedia: str = Field(...)
+    isComplete: bool = Field(...)
+
+
+class Units(BaseModel):
+    unitId: int = Field(...)
+    unitName: str = Field(...)
+    unitSlug: str = Field(...)
+    unitDesc: str = Field(...)
+    unitData: str = Field(...)
+    sections: list[UnitSections] = Field(...)
+
+
+class Modules(BaseModel):
+    moduleId: int = Field(...)
+    moduleName: str = Field(...)
+    moduleSlug: str = Field(...)
+    moduleDesc: str = Field(...)
+    moduleData: str = Field(...)
+    sections: list[UnitSections] = Field(...)
